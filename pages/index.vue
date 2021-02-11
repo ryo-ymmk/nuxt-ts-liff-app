dc350499c19b1ea3d9032dc126df38e6515cb308
ryo-ymmk <ryclock@gmail.com>
2021-02-11 09:39:17 +0900

    とりあえず下地作った

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">liff-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p>いぬのかわいさを享受しろ</p>
     </div>
   </div>
 </template>
@@ -39,27 +22,5 @@ export default Vue.extend({})
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
